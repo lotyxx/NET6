696856a510048007e1e2b3659e0be6d502826bcd
lotyxx <91486591+lotyxx@users.noreply.github.com>
2022-04-25 11:32:48 +0200

    prehozeni
    prehozeni cisel

---
+++ PlayGround/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int cislo = -13+22;
+double desetinne_cislo = 308.1222;
+string retezec = "ahoj svete";
+char pismenko = 'A';
+Console.WriteLine(10+222);
+
+int a = 10;
+int b = 20;
+int c;
+c = b;
+b = a;
+a = c;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+
+
+Console.ReadKey();
