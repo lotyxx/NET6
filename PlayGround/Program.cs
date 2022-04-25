47f90c782ec3d4e90f7c92525f0ddbcd01c9fe60
lotyxx <91486591+lotyxx@users.noreply.github.com>
2022-04-25 11:44:54 +0200

    Prevod teploty
    vypocet teploty

--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/* Console.WriteLine("Hello, World!");
 int cislo = -13+22;
 double desetinne_cislo = 308.1222;
 string retezec = "ahoj svete";
 char pismenko = 'A';
-Console.WriteLine(10+222);
+Console.WriteLine(10+222); */
 
-int a = 10;
+/*int a = 10;   prvni uloha
 int b = 20;
 int c;
 c = b;
@@ -18,3 +18,16 @@ Console.WriteLine(b);
 
 
 Console.ReadKey();
+*/
+
+double teplota;
+double prevod;
+Console.WriteLine("Zadej teplotu ve stupnich celsia");
+teplota = int.Parse(Console.ReadLine());
+prevod = ((1.8 * teplota) + 32);
+Console.WriteLine(prevod);
+Console.ReadKey();
+
+
+
+
