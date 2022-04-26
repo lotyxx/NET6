7ce509985e97fbeba7e79df8b95958acc3ce1def
lotyxx <91486591+lotyxx@users.noreply.github.com>
2022-04-26 14:27:11 +0200

    Cars

---
+++ PlayGround/Model/Cars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround.Model
+{
+    public class Cars
+    {
+        public string SPZ { get; set; }
+        public string Barva { get; set; }
+
+        public string Znacka { get; set; }
+
+        public string NajeteKm { get; set; }
+
+
+    }
+}
