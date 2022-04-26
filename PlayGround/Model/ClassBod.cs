1fb83d3238bdf26d8714550eb1a5f343f77622fe
lotyxx <91486591+lotyxx@users.noreply.github.com>
2022-04-26 10:48:02 +0200

    vypocetobsahuobdelnika

---
+++ PlayGround/Model/ClassBod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround.Model
+{
+    internal class ClassBod
+    {
+        public ClassBod(double _x, double _y)
+        {
+            X = _x;
+            Y = _y;
+        }
+
+        public ClassBod()
+        {
+
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"x : {X} y: {Y}");
+        }
+
+        public double VypocetObdelnika()
+        {
+            double  obsah = p1.X * p2.X;
+            return obsah;
+
+        }
+
+    }
+}
---
+++ PlayGround/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround.Model
+{
+    internal class Person
+    {
+        public string Name { get;set; } //zapis a cteni get a set
+        DateTime DateOfBirth { get; set; }
+    }
+}
