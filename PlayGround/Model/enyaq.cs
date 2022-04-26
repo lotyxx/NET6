b4bc39113dbf1957c0c9f7b8899d3b618a80413e
lotyxx <91486591+lotyxx@users.noreply.github.com>
2022-04-26 14:13:08 +0200

    vlastnosti

---
+++ PlayGround/Model/enyaq.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround.Model
+{
+    public class enyaq
+    {
+
+
+
+
+        public string spz()
+            {
+             }
+        public string  barva()
+        {
+
+        }
+        public public double pocetkm ()
+         {
+         }
+        public string barva ()
+         {
+
+         }
+    }
+}
